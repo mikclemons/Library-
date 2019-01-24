61d967a549c77822b72fc00e58fbc96487537833
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Air.local>
2019-01-24 16:01:51 -0500

    Create accessor methods for each of the instance variables.

--- src/com/company/Book.java
+++ src/com/company/Book.java
@@ -16,5 +16,32 @@ public class Book {
     private String title;
     private String author;
     private int numberOfPages;
-    private String genre; 
+    private String genre;
+
+    public void setTitle(String bookTitle){
+        title = bookTitle;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public void setAuthor(String bookAuthor){
+        author = bookAuthor;
+    }
+    public String getAuhor(){
+        return author;
+    }
+    public void setNumberOfPages(int bookNumberOfPages){
+        numberOfPages = bookNumberOfPages;
+    }
+
+    public int getNumberOfPages(){
+        return numberOfPages;
+    }
+    public void setGenre(String bookGenre){
+        genre = bookGenre;
+    }
+    public String getGenre(){
+        return genre; 
+    }
+
 }
